17d9c8143bd771b571bab0abf70eafc0b8ca2a88
Petr Ptacek <petr.ptacek@olc.cz>
2025-05-27 13:21:38 +0200

    add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    indentation: 2,
+    "string-quotes": "double",
+    "color-hex-case": "lower",
+  },
+};
