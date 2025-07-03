e3b4e9e8a84891579b065c9f81abf48ef8e2d799
Petr Ptacek <petr.ptacek@olc.cz>
2025-07-03 11:10:26 +0200

    update packages

--- playground/App.vue
+++ playground/App.vue
@@ -7,19 +7,19 @@ import { VueResizeContainer, VueResizeContainerSection } from "vue-resize-contai
     <VueResizeContainer direction="horizontal" origin="alpha">
       <template #sectionAlpha>
         <VueResizeContainerSection>
-          <div style="background-color: blueviolet; height: 100%; display: flex; justify-content: center; align-items: center; font-size: 24px;">
+          <div style="background-color: white; height: 100%; display: flex; justify-content: center; align-items: center; font-size: 24px;">
             Section Alpha
           </div>
         </VueResizeContainerSection>
       </template>
       <template #sectionBeta>
         <VueResizeContainerSection>
-          <div style="background-color: cyan;">
-            <div style="height: 100%; display: flex; justify-content: center; align-items: center; font-size: 24px;">
-              <div style="width: 400px; height: 200px; background-color: red; display: flex; justify-content: center; align-items: center; text-transform: uppercase;">
-                BOx
-              </div>
-            </div>
+          <div style="background-color: white;">
+<!--            <div style="height: 100%; display: flex; justify-content: center; align-items: center; font-size: 24px;">-->
+<!--              <div style="width: 400px; height: 200px; background-color: red; display: flex; justify-content: center; align-items: center; text-transform: uppercase;">-->
+<!--                BOx-->
+<!--              </div>-->
+<!--            </div>-->
           </div>
         </VueResizeContainerSection>
       </template>
